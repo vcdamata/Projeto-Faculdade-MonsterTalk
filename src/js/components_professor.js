c5a017df01bc6ea1796d0d39d26e1f41d05e6558
Bruce <andrebgsi@gmail.com>
2025-05-13 11:48:47 -0300

    feat: Ajustar pastas das areas internas #50

--- src/js/components_professor.js
+++ src/js/components_professor.js
@@ -1,13 +1,30 @@
-function loadComponent(id, path) {
-  fetch(path)
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    })
-    .catch(err => console.error(`Erro ao carregar ${path}:`, err));
+function getScriptDir() {
+  // Caminho base do script atual (carregado no HTML)
+  const script = document.currentScript || document.querySelector('script[src*="components_professor.js"]');
+  const path = script.getAttribute('src');
+  return path.substring(0, path.lastIndexOf('/') + 1);
 }
 
-// Carregar componentes
+function loadComponent(id, relativePath) {
+  const basePath = getScriptDir();
+  const fullPath = basePath + relativePath;
+
+  fetch(fullPath)
+    .then(res => {
+      if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+      return res.text();
+    })
+    .then(data => {
+      const element = document.getElementById(id);
+      if (element) {
+        element.innerHTML = data;
+      } else {
+        console.warn(`Elemento com ID '${id}' não encontrado.`);
+      }
+    })
+    .catch(err => console.error(`Erro ao carregar ${fullPath}:`, err));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   loadComponent("header", "../components/header_professor.html");
   loadComponent("sidebar", "../components/sidebar_professor.html");
